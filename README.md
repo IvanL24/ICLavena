1736e6428a3a778958c64696a4ab247469a6ca55
Ivan Lavena <lavenaivan@gmail.com>
2020-12-22 09:01:53 -0700

    updated menu

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ About me:
   - make it more prettier
   - add contents under home button
   - link projects from GitHub
+  - change the font (current font is too loud)
+  - fix code quality
 
 ## possible to do list
   - Try recreating portfolio by using EJS and SCSS 
+
+## notes 
+  - added colapsable menu(still needs work)
+  - make plan on how to make it make more user friendly
--- js/client.js
+++ js/client.js
@@ -1,9 +1,60 @@
-const home = document.querySelector('.home');
+var card =  document.getElementById('activator');
 
-home.addEventListener('click', function() {
+var tl =  gsap.timeline({defaults: {ease: "power2.inOut"}})
 
-  const drpdwn = document.querySelector('.home .drpdown');
-console.log(drpdwn)
+var toggle = false;
+
+tl.to('.activator', {
+  background: '#805ad5',
+    'borderRadius': '50% 50% 0 0',
+    'transform': 'translateY(4px)'
+})
+tl.to('nav', {
+  'clipPath': 'ellipse(50% 200% at 50% 50%)'
+}, '0.5s')
+tl.to('nav li a i', {
+  opacity: 1,
+  transform: 'translateY(0)',
+  stagger: 0.005
+})
+tl.pause();
+
+card.addEventListener('click', () => {
+  toggle = !toggle;
+  if (toggle ? tl.play() : tl.reverse());
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const home = document.querySelector('.home');
+
+// home.addEventListener('click', function() {
+
+//   const drpdwn = document.querySelector('.home .drpdown');
+// console.log(drpdwn)
 
 
   //1
@@ -39,4 +90,4 @@ console.log(drpdwn)
 // }
 
 
-})
+// })
